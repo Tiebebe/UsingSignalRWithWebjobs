44a0e4706d73c573278e0d6d49a0c8a4b9dec536
Jerrie Pelser <jerrie@jerriepelser.com>
2015-07-06 17:58:02 +0700

    Initial Commit

---
+++ Website/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.AspNet.SignalR;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json;
+using Website.Models;
+
+namespace Website.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> CreateJob()
+        {
+            var jobId = Guid.NewGuid();
+
+            var storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["QueueStorageConnectionString"].ConnectionString);
+            var cloudQueueClient = storageAccount.CreateCloudQueueClient();
+
+            var queue = cloudQueueClient.GetQueueReference("jobqueue");
+            queue.CreateIfNotExists();
+
+            CloudQueueMessage message = new CloudQueueMessage(JsonConvert.SerializeObject(jobId));
+            await queue.AddMessageAsync(message);
+
+            return RedirectToAction("JobStatus", new {jobId});
+        }
+
+        public ActionResult JobStatus(Guid jobId)
+        {
+            var viewModel = new JobStatusViewModel
+            {
+                JobId = jobId
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult ProgressNotification(Guid jobId, int progress)
+        {
+            var connections = JobProgressHub.GetUserConnections(jobId);
+
+            if (connections != null)
+            {
+                foreach (var connection in connections)
+                {
+                    // Notify the client to refresh the list of connections
+                    var hubContext = GlobalHost.ConnectionManager.GetHubContext<JobProgressHub>();
+                    hubContext.Clients.Clients(new[] { connection }).updateProgress(progress);
+                }
+            }
+
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ Website/Models/JobStatusViewModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Website.Models
+{
+    public class JobStatusViewModel
+    {
+        public Guid JobId { get; set; }
+    }
+}
